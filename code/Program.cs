4ef6932cf3cebe959e5480fb5b47cda1fd17166f
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-03-26 21:05:05 -0700

    Updated Program.cs test cases
    Added xorProblem() and boxCrossProblem() test cases

--- code/Program.cs
+++ code/Program.cs
@@ -2,18 +2,13 @@
 using CsEdu_3_Perceptron;
 
 /**
- * This class contains the main entry point for this project. It creates a new Network and attempts to 
- * teach the network to recognize XOR. After training the Network, it prints the result of feeding the 
- * Network all four possible binary inputs and terminates. This class was written primarily to benchmark 
- * the speed of regular steepest gradient descent and memoizized SGD (backpropogation).
- * 
  * BENCHMARK RESULTS:
- * Surface Pro 3, Intel Core i5
- *    w/o backpropogation: 482ms
- *    w/  backpropogation: 256ms
- * Lenovo Yoga Pro 2, Intel Core i7
- *    w/o backpropogation: 371ms
- *    w/  backpropogation: 147ms
+ * Surface Pro 3, Core i5:       xorProblem() | boxCrossProblem()
+ *    w/o backpropogation:       482ms        | 64ms
+ *    w/  backpropogation:       256ms        | 47ms
+ * Lenovo Yoga Pro 2, Core i7,   xorProblem() | boxCrossProblem()
+ *    w/o backpropogation:       371ms        | 51ms
+ *    w/  backpropogation:       147ms        | 42ms
  * 
  * author: Kevin Zhang
  * version: 201503.26
@@ -22,8 +17,11 @@ class Program
 {
    static void Main(string[] args)
    {
+      boxCrossProblem();
+   }
+
+   static void xorProblem() {
       Network net = new Network(2, 5, 5, 1);
-      net.feed(new float[] { 0.0f, 0.0f });
 
       int N = 4;
       float[][] input = new float[N][];
@@ -40,6 +38,45 @@ class Program
 
       input[3] = new float[] { 1.0f, 1.00f };
       output[3] = new float[] { 0.0f };
+
+      net.train(N, input, output);
+      for (int n = 0; n < N; n++)
+         Console.WriteLine(string.Join(" ", input[n]) + " -> " + string.Join(" ", net.feed(input[n])));
+   }
+
+   static void boxCrossProblem()
+   {
+      Network net = new Network(2, 5, 5, 2);
+
+      int N = 2;
+      float[][] input = new float[N][];
+      float[][] output = new float[N][];
+
+      input[0] = new float[] { // cross (X)
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 
+                    0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 
+                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 
+                    0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 
+                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 
+                    0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 
+                    0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                };
+      output[0] = new float[] { 1.0f, 0.0f };
+
+      input[1] = new float[] { // box (O)
+                    1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 
+                    1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 
+                };
+      output[1] = new float[] { 0.0f, 1.0f };
 
       net.train(N, input, output);
       for (int n = 0; n < N; n++)
