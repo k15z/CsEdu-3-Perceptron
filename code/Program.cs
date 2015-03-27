66c42c6c4ec0e219e7416318761b2ea8678e7479
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-03-27 09:40:04 -0700

    Added OCR problem

--- code/Program.cs
+++ code/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CsEdu_3_Perceptron;
+using System.Drawing;
 
 /**
  * BENCHMARK RESULTS:
@@ -17,7 +18,7 @@ class Program
 {
    static void Main(string[] args)
    {
-      boxCrossProblem();
+      ocrProblem();
    }
 
    static void xorProblem() {
@@ -81,5 +82,35 @@ class Program
       net.train(N, input, output);
       for (int n = 0; n < N; n++)
          Console.WriteLine(string.Join(" ", input[n]) + " -> " + string.Join(" ", net.feed(input[n])));
+   }
+
+   static void ocrProblem()
+   {
+      int N = 26;
+      float[][] input = new float[N][];
+      float[][] output = new float[N][];
+      for (char c = 'A'; c < 'Z'; c++)
+      {
+         int n = (int)(c - 'A');
+         Bitmap img = new Bitmap(Image.FromFile("img/" + c + ".bmp"));
+
+         int i = 0;
+         input[n] = new float[img.Height * img.Width];
+         for (int row = 0; row < img.Height; row++)
+            for (int col = 0; col < img.Width; col++)
+               input[n][i++] = img.GetPixel(col, row).GetBrightness();
+
+         output[n] = new float[N];
+         output[n][n] = 1.0f;
+      }
+
+      int S, R, Q, P;
+      S = input[0].Length;
+      P = output[0].Length;
+      R = (new Random()).Next(P, S/2);
+      Q = (new Random()).Next(P, S/3);
+      Console.WriteLine(S + " -> " + R + " -> " + Q + " -> " + P);
+      Network net = new Network(S, R, Q, P);
+      net.train(N, input, output);
    }
 }
