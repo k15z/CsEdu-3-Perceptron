a2dd9853189c041d16c0e499862b38bb39cda754
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-03-26 19:46:45 -0700

    Updated Program.cs
    Modified code for benchmarking

--- code/Program.cs
+++ code/Program.cs
@@ -5,11 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * This class contains the main entry point for this project. It creates a new Network and attempts to 
+ * teach the network to recognize XOR. After training the Network, it prints the result of feeding the 
+ * Network all four possible binary inputs and terminates. This class was written primarily to benchmark 
+ * the speed of regular steepest gradient descent and memoizized SGD (backpropogation).
+ * 
+ * BENCHMARK RESULTS:
+ * Surface Pro 3, Intel Core i5
+ *    w/o backpropogation: 482ms
+ *    w/  backpropogation: 256ms
+ * Lenovo Yoga Pro 2, Intel Core i7
+ *    w/o backpropogation: 371ms
+ *    w/  backpropogation: 147ms
+ * 
+ * author: Kevin Zhang
+ * version: 201503.26
+ */
 class Program
 {
    static void Main(string[] args)
    {
-      Network net = new Network(2, 5, 4, 1);
+      Network net = new Network(2, 5, 5, 1);
       net.feed(new float[] { 0.0f, 0.0f });
 
       int N = 4;
@@ -28,15 +45,8 @@ class Program
       input[3] = new float[] { 1.0f, 1.00f };
       output[3] = new float[] { 0.0f };
 
-      while (true)
-      {
-         net.train(N, input, output);
-         for (int n = 0; n < N; n++)
-         {
-            Console.WriteLine(string.Join(" ", input[n]) + " -> " + string.Join(" ", net.feed(input[n])));
-         }
-         Console.WriteLine("Press \"Enter\" to continue training...");
-         Console.ReadLine();
-      }
+      net.train(N, input, output);
+      for (int n = 0; n < N; n++)
+         Console.WriteLine(string.Join(" ", input[n]) + " -> " + string.Join(" ", net.feed(input[n])));
    }
 }
